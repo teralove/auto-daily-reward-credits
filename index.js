cf2b449869ec00bdd61c255d5b6e6ffda9a2cdc4
teralove <terascripts@gmail.com>
2018-04-25 07:46:25 -0400

    Update 1.1

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-// vers 1.0.0
-
 module.exports = function AutoDailyRewardCredits(dispatch) {
-    dispatch.hook('S_LOGIN', 1, (event) => {
+    dispatch.hook('S_LOGIN', (event) => {
         dispatch.toServer('C_REQUEST_RECV_DAILY_TOKEN', 1);
     })
 }

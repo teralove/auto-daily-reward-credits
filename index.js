89e4a6016c4e926809f0a3806705e4de5c8ad8d3
seraph <30970630+seraphinush-gaming@users.noreply.github.com>
2018-05-02 10:16:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function AutoDailyRewardCredits(dispatch) {
-    dispatch.hook('S_LOGIN', (event) => {
+    dispatch.hook('S_LOGIN', 'raw', () => {
         dispatch.toServer('C_REQUEST_RECV_DAILY_TOKEN', 1);
     })
 }
